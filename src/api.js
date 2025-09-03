bcf6c39beca654e8776ed07cdd7b3f2f11df63c5
keerthikaa27 <keerthikanimmagadda@gmail.com>
2025-09-03 23:55:42 +0530

    Update

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
+const BASE_URL = "https://energy-forecast-backend.onrender.com/"; 
+
 export async function getPrediction(values, horizon = 6) {
-  const res = await fetch(`http://localhost:10000/predict/${horizon}`, {
+  const res = await fetch(`${BASE_URL}/predict/${horizon}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ values }),
   });
   const data = await res.json();
-  return data.forecast; // array of predicted values
+  return data.forecast;
 }
 
